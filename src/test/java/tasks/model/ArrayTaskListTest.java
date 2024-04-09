0fb5da7b5ac1c9270bb6bf39eb68c6018e1a93fe
qwerty <therealthunderstorm6@gmail.com>
2024-04-09 13:29:58 +0300

    Numa test coverage mai trebe?

--- src/test/java/tasks/model/ArrayTaskListTest.java
+++ src/test/java/tasks/model/ArrayTaskListTest.java
@@ -19,33 +19,60 @@ public class ArrayTaskListTest {
 
     @Test
     void testAdd() {
-        Task task = new Task("Test", new Date());
-        taskList.add(task);
-        assertEquals(1, taskList.size());
-        assertEquals(task, taskList.getTask(0));
-    }
-
-    @Test
-    void testRemove() {
-        Task task = new Task("Test", new Date());
-        taskList.add(task);
-        taskList.remove(task);
-        assertEquals(0, taskList.size());
-    }
-
-    @Test
-    void testSize() {
-        Task task1 = new Task("Test1", new Date());
-        Task task2 = new Task("Test2", new Date());
+        // Test case 1
+        Task task1 = new Task("Task", new Date(10), new Date(15), 2);
         taskList.add(task1);
-        taskList.add(task2);
-        assertEquals(2, taskList.size());
-    }
+        assertEquals(1, taskList.size());
+        assertEquals(task1, taskList.getTask(0));
+        System.out.println("Test case 1 passed");
 
-    @Test
-    void testGetTask() {
-        Task task = new Task("Test", new Date());
-        taskList.add(task);
-        assertEquals(task, taskList.getTask(0));
+        // Test case 2
+        assertThrows(IllegalArgumentException.class, () -> {
+            Task task2 = new Task("Task", new Date(-1), new Date(15), 2);
+            taskList.add(task2);
+        });
+        System.out.println("Test case 2 passed");
+
+        // Test case 3
+        assertThrows(IllegalArgumentException.class, () -> {
+            Task task3 = new Task("Task", new Date(10), new Date(-1), 2);
+            taskList.add(task3);
+        });
+        System.out.println("Test case 3 passed");
+
+        // Test case 4
+        assertThrows(IllegalArgumentException.class, () -> {
+            Task task4 = new Task("Task", new Date(-1), new Date(-1), 2);
+            taskList.add(task4);
+        });
+        System.out.println("Test case 4 passed");
+
+        // Test case 5
+        Task task5 = new Task("Task", new Date(1), new Date(15), 2);
+        taskList.add(task5);
+        assertEquals(2, taskList.size());
+        assertEquals(task5, taskList.getTask(1));
+        System.out.println("Test case 5 passed");
+
+        // Test case 6
+        assertThrows(IllegalArgumentException.class, () -> {
+            Task task6 = new Task("Task", new Date(-1), new Date(15), 2);
+            taskList.add(task6);
+        });
+        System.out.println("Test case 6 passed");
+
+        // Test case 7
+        Task task7 = new Task("Task", new Date(15), new Date(1), 2);
+        taskList.add(task7);
+        assertEquals(3, taskList.size());
+        assertEquals(task7, taskList.getTask(2));
+        System.out.println("Test case 7 passed");
+
+        // Test case 8
+        assertThrows(IllegalArgumentException.class, () -> {
+            Task task8 = new Task("Task", new Date(15), new Date(-1), 2);
+            taskList.add(task8);
+        });
+        System.out.println("Test case 8 passed");
     }
 }
