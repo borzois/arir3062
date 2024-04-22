b8befaeb4867688cc1c07cefb7646489e416f8a8
qwerty <therealthunderstorm6@gmail.com>
2024-04-23 01:57:41 +0300

    Am facut pentru arrayTaskList atm

--- src/test/java/tasks/model/WithMockito/TasksServiceTested.java
+++ src/test/java/tasks/model/WithMockito/TasksServiceTested.java
@@ -1,4 +1,45 @@
-package tasks.model.WithMockito;
+package tasks.services.WithMockito;
 
-public class TasksServiceTested {
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import tasks.model.Task;
+import tasks.services.TasksService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class TasksServiceTested {
+
+    @Test
+    void testGetIntervalInHoursOneHour() {
+        Task task = Mockito.mock(Task.class);
+        when(task.getRepeatInterval()).thenReturn(3600); // 1 hour in seconds
+
+        TasksService service = new TasksService(null);
+        String interval = service.getIntervalInHours(task);
+
+        assertEquals("01:00", interval);
+    }
+
+    @Test
+    void testGetIntervalInHoursTwoAndHalfHours() {
+        Task task = Mockito.mock(Task.class);
+        when(task.getRepeatInterval()).thenReturn(9000); // 2.5 hours in seconds
+
+        TasksService service = new TasksService(null);
+        String interval = service.getIntervalInHours(task);
+
+        assertEquals("02:30", interval);
+    }
+
+    @Test
+    void testGetIntervalInHoursZeroHours() {
+        Task task = Mockito.mock(Task.class);
+        when(task.getRepeatInterval()).thenReturn(0); // 0 hours in seconds
+
+        TasksService service = new TasksService(null);
+        String interval = service.getIntervalInHours(task);
+
+        assertEquals("00:00", interval);
+    }
 }
